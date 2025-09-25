2c57dc5e9b652a751d2899c234d879b4270821f5
trisbaena <46066851+trisbaena@users.noreply.github.com>
2025-09-25 07:25:22 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Lois-Halphiniria-calculator/', // ต้องตรงชื่อ repo เป๊ะ (Case-sensitive)
 })
